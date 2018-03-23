f5d7ee6fc25afb4ee9b6a7e8ba2109480595ad9f
Zarukin <37689537+Zarukin@users.noreply.github.com>
2018-03-23 11:16:57 +0100

    Update to_dimacs.h

--- to_dimacs.h
+++ to_dimacs.h
@@ -2,11 +2,15 @@
 #define TO_DIMACS_H
 
 int coord_to_number(unsigned int l, unsigned int c, unsigned int n);
+//renvoie id=taille d eligne carré fois l + taille de ligne *c + n 
 
 int number_to_coord(unsigned int number, unsigned int *l, unsigned int *c, unsigned int *n);
+// renvoie la colone 
 
 void read_dimacs_file(char *dimacs_filename);
+// lire un fichier d'expression logique voir internet format dimacs
 
 void write_dimacs_file(char *dimacs_filename);
+// ecrire toute les clauses 
 
 #endif
