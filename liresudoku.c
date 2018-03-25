d4607d227796b38bd42783f3b71a00a51afbecd2
Requiem958 <requiemandsahariel@gmail.com>
2018-03-25 23:41:45 +0200

    Modification entêtes

--- liresudoku.c
+++ liresudoku.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include "sudoku.h"
+#include "liresudoku.h"
 
 //Local function to see if a is a perfect_square = a k*k, k in N
 int is_perfect_square(int a){
